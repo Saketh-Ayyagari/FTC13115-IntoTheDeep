4f13fa647ce91d94abdab23994e130b465d9c12f
jordani268 <jordan57875@gmail.com>
2024-09-21 22:58:36 -0400

    testing this

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/tutorial.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/tutorial.java
@@ -1,4 +1,28 @@
 package org.firstinspires.ftc.robotcontroller.internal;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
-public class tutorial {
+@TeleOp(name = "FTC13115")
+
+public class tutorial extends OpMode
+{
+    DcMotor motor;
+
+    @Override
+    public void init()
+    {
+        motor = hardwareMap.get(DcMotor.class, ""); // add device name from motors in control hub
+        telemetry.addData("Hardware: ", "Initialized");
+    }
+    @Override
+    public void loop()
+    {
+        float x = gamepad1.left_stick_x;
+        if (gamepad1.left_stick_x > 0)
+        {
+            motor.setPower(x);
+        }
+        motor.setPower(0); // turns off the motor
+    }
 }
