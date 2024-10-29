ee997c56a58b0f7b24438e85738636aed9a8fe17
jordani268 <jordan57875@gmail.com>
2024-10-29 14:24:17 -0400

    for branch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpticalSens.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpticalSens.java
@@ -1,4 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
-public class OpticalSens {
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@TeleOp
+public class OpticalSens extends OpMode {
+    DistanceSensor opticalsensor; // creates a location in memory for a distance sensor
+    @Override
+    public void init()
+    {
+        //  places the location in memory as an actual distance sensor
+        opticalsensor = hardwareMap.get(DistanceSensor.class, "distance sensor");
+    }
+
+    public void loop()  // telemetry goes in the loop, must be continuously updated
+    {        double value = opticalsensor.getDistance(DistanceUnit.INCH);
+        telemetry.addData("Distance:", value);
+        telemetry.update();
+    }
 }
