2df6ce1a299bba6121f7d4cbca538de6c3a1ed29
jordani268 <jordan57875@gmail.com>
2024-10-29 22:54:51 -0400

    color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSens.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSens.java
@@ -1,4 +1,50 @@
 package org.firstinspires.ftc.teamcode;
 
-public class ColorSens {
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.ColorRangeSensor;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.LightSensor;
+import com.qualcomm.robotcore.hardware.OpticalDistanceSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class ColorSens
+{
+    ColorSens colorSens;
+
+    @Override
+    public void init()
+    {
+        colorSens = hardwareMap.get(ColorSens.class, "colorsensor");
+    }
+
+    @Override
+    public void loop()
+    {
+        int red = colorSens.red();
+        int green = colorSens.green();
+        int blue = colorSens.blue();
+
+        telemetry.addData("Red", red);
+        telemetry.addData("Green", green);
+        telemetry.addData("Blue", blue);
+
+        // Display the dominant color
+        if (red > green && red > blue) {
+            telemetry.addData("Detected Color", "Red");
+        } else if (green > red && green > blue) {
+            telemetry.addData("Detected Color", "Green");
+        } else if (blue > red && blue > green) {
+            telemetry.addData("Detected Color", "Blue");
+        } else {
+            telemetry.addData("Detected Color", "Unknown");
+        }
+
+        telemetry.update();
+    }
+
+    }
 }
