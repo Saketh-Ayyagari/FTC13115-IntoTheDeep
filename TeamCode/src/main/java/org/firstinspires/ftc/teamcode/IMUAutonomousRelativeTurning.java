ff221ecd13edca8e41a2c9ab53bba28a9c3b2107
Saketh Ayyagari <sakethsarma07@gmail.com>
2024-11-08 00:25:22 -0500

    adding more documentation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUAutonomousRelativeTurning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUAutonomousRelativeTurning.java
@@ -12,16 +12,17 @@ import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 
 @Autonomous(name="IMUAutonomousRelativeTurning", group="Autonomous")
 public class IMUAutonomousRelativeTurning extends LinearOpMode {
+    // initializing robot, IMU, and IMU parameter objects
     private ElapsedTime runtime = new ElapsedTime();
     private Robot drivetrain;
     private IMU.Parameters myIMUParameters;
     private IMU imu;
-
-    private static final double MAX_POWER = 0.5;
+    // max speed the robot will move
+    private static final double MAX_POWER = 0.3;
 
     @Override
     public void runOpMode() {
-        // Initial setup for IMU
+        // Initial setup for IMU--initializes parameters based on IMU orientation
         myIMUParameters = new IMU.Parameters(
                 new RevHubOrientationOnRobot(
                         RevHubOrientationOnRobot.LogoFacingDirection.LEFT,
@@ -30,6 +31,7 @@ public class IMUAutonomousRelativeTurning extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
+        // creating and initializing Robot object
         drivetrain = new Robot(MAX_POWER);
         drivetrain.init(hardwareMap);
 
@@ -42,11 +44,9 @@ public class IMUAutonomousRelativeTurning extends LinearOpMode {
         runtime.reset();
 
         // Example action: Turn 90 degrees clockwise and move forward
-        turnRelative(90);  // Turn 90 degrees clockwise
-        drivetrain.moveRobotwEncoders("forward", 10);
-
-        turnRelative(90);  // Turn another 90 degrees (total 180 degrees from start)
-        drivetrain.moveRobotwEncoders("forward", 10);
+        turnRelative(90);  // Turn 90 degrees counterclockwise
+        // positive = counterclockwise
+        // negative = clockwise
     }
 
     /**
@@ -54,33 +54,29 @@ public class IMUAutonomousRelativeTurning extends LinearOpMode {
      * @param relativeDegrees The degrees to turn relative to the current heading.
      */
     public void turnRelative(double relativeDegrees) { // must be between -180 and 180
-        // Create an object to receive the IMU angles
-        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-
-        // reset the yaw
         imu.resetYaw();
-
         // Keep turning until the robot heading is greater than the target heading
-        while (orientation.getYaw() < relativeDegrees) {
-            // Simple control: turn left or right based on the heading difference
-            if (getHeading() > 0) {
+        while (Math.abs(getHeading()) < Math.abs(relativeDegrees)) {
+            // Simple control: turn left or right based on the sign of the heading value
+            if (relativeDegrees > 0) {
                 drivetrain.powerMotors(0, MAX_POWER, 0); // Turn right
-            } else {
-                drivetrain.powerMotors(0, -MAX_POWER, 0); // Turn left
+            }
+            else{
+                drivetrain.powerMotors(0, -MAX_POWER, 0);
             }
 
-            telemetry.addData("Current Heading", orientation.getYaw());
-            telemetry.addData("Target Heading", orientation.getYaw());
+            telemetry.addData("Current Heading", getHeading());
+            telemetry.addData("Target Heading", relativeDegrees);
             telemetry.update();
         }
         // Stop the robot once the target heading is reached
-        drivetrain.powerMotors(0, 0, 0); // Stop motors
+        drivetrain.brake();
     }
 
     /**
      * Get the current heading of the robot in degrees.
      */
-    public double getHeading() {
+    public double getHeading() { //
         YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
         return orientation.getYaw(AngleUnit.DEGREES);
     }
